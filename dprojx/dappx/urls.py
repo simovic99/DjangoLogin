756fb24660d6b84d6915145fa185655f57e2d355
simovic99 <simovic.petar99@gmail.com>
2020-06-18 15:06:56 +0200

    first commit

---
+++ dprojx/dappx/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from dappx import views
+
+# SET THE NAMESPACE!
+app_name = 'dappx'
+
+
+urlpatterns=[
+    url(r'^register/$',views.register,name='register'),
+    url(r'^user_login/$',views.user_login,name='user_login'),
+]
